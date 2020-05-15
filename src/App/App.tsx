79ae10df7aad9e53c0333efdba1394db6ea63f8b
geetanshu2502 <geetanshu2502@gmail.com>
2020-05-15 18:26:09 +0530

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { IonApp } from "@ionic/react";
+import * as AppGeneral from "../socialcalc/AppGeneral";
+import { DATA } from "../app-data.js";
+
+import "./App.css";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+/* Theme variables */
+import "../theme/variables.css";
+
+const App: React.FC = () => (
+  <IonApp>
+    <h2>Hello</h2>
+  </IonApp>
+);
+
+export default App;
